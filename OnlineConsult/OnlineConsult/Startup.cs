63d9ce47a343be379ce6ddb573b8b3c53e70bc2c
Niall Quinn <niall.quinn.27@gmail.com>
2015-10-17 13:47:57 +0100

    Initialise Project

---
+++ OnlineConsult/OnlineConsult/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineConsult.Startup))]
+namespace OnlineConsult
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
